a8ae7a8ea151b532af3eee2fd9c520b72f9181a4
jimin <jjimini98@naver.com>
2021-05-03 11:16:12 +0900

    proceeding hw1.py (이진탐색 추가공부)

---
+++ 2.정렬,탐색/hw1.py
@@ -0,0 +1,18 @@
+# 이진탐색
+
+# 시간초과..;; 
+# def solution(L,x):
+#     for l in L:
+#         if l<x : continue
+#         if x not in L: return -1
+#         if x == l: return L.index(l)
+
+
+ 
+def solution(L, x):
+   mid = ()
+
+L = [2, 3, 5, 6, 9, 11, 15]
+x = 6
+
+print(solution(L,x))
